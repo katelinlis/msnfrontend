541ae211aaee60d79081a999b08c6c99deb06e92
Katelinlis <katelinlis1999@gmail.com>
2021-08-25 09:46:04 +0300

    Install recaptcha for bots and fix friends

--- views/js/login.js
+++ views/js/login.js
@@ -4,28 +4,38 @@ function register() {
   let savePassword = document.getElementById('SavePassword').value;
   console.log(savePassword);
   if (username && password)
-    axios
-      .post('https://social.katelinlis.xyz/api/auth/login', {
-        username,
-        password,
-      })
-      .then(function (response) {
-        if (response.data && response.data.token) {
-          window.localStorage.setItem('InputUsername', username);
-          if (savePassword === 'on') {
-            window.localStorage.setItem('InputPassword', password);
-          }
-          window.localStorage.setItem('token', response.data.token);
-          setCookie('token', response.data.token);
-          document.location.href =
-            'https://social.katelinlis.xyz/user/' + response.data.userid;
-        }
-        console.log(response);
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
+    grecaptcha.ready(function () {
+      grecaptcha
+        .execute('6LfyLyIcAAAAALaiGsWJhlGVavDKRU-2BWmtkzaE', {
+          action: 'submit',
+        })
+        .then(function (recaptcha) {
+          axios
+            .post('https://social.katelinlis.xyz/api/auth/login', {
+              username,
+              password,
+              recaptcha,
+            })
+            .then(function (response) {
+              if (response.data && response.data.token) {
+                window.localStorage.setItem('InputUsername', username);
+                if (savePassword === 'on') {
+                  window.localStorage.setItem('InputPassword', password);
+                }
+                window.localStorage.setItem('token', response.data.token);
+                setCookie('token', response.data.token);
+                document.location.href =
+                  'https://social.katelinlis.xyz/user/' + response.data.userid;
+              }
+              console.log(response);
+            })
+            .catch(function (error) {
+              console.log(error);
+            });
+        });
+    });
 }
+
 setTimeout(() => {
   console.log('ddd');
   document.getElementById('InputUsername').value =
