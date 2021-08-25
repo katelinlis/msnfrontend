b92db5e9ab55f6234e8d9dbc1d18d8df6c1ba0d2
Katelinlis <katelinlis1999@gmail.com>
2021-08-25 08:23:20 +0300

    Сохранение пароля и доработка стилизации страницы авторизации

--- views/js/login.js
+++ views/js/login.js
@@ -1,18 +1,35 @@
 function register() {
-  axios
-    .post('https://social.katelinlis.xyz/api/auth/login', {
-      username: document.getElementById('InputUsername').value,
-      password: document.getElementById('InputPassword').value,
-    })
-    .then(function (response) {
-      if (response.data && response.data.token)
-        window.localStorage.setItem('token', response.data.token);
-      setCookie('token', response.data.token);
-      document.location.href =
-        'https://social.katelinlis.xyz/user/' + response.data.userid;
-      console.log(response);
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
+  let username = document.getElementById('InputUsername').value;
+  let password = document.getElementById('InputPassword').value;
+  let savePassword = document.getElementById('SavePassword').value;
+  console.log(savePassword);
+  if (username && password)
+    axios
+      .post('https://social.katelinlis.xyz/api/auth/login', {
+        username,
+        password,
+      })
+      .then(function (response) {
+        if (response.data && response.data.token) {
+          window.localStorage.setItem('InputUsername', username);
+          if (savePassword === 'on') {
+            window.localStorage.setItem('InputPassword', password);
+          }
+          window.localStorage.setItem('token', response.data.token);
+          setCookie('token', response.data.token);
+          document.location.href =
+            'https://social.katelinlis.xyz/user/' + response.data.userid;
+        }
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
 }
+setTimeout(() => {
+  console.log('ddd');
+  document.getElementById('InputUsername').value =
+    window.localStorage.getItem('InputUsername');
+  document.getElementById('InputPassword').value =
+    window.localStorage.getItem('InputUsername');
+}, 200);
